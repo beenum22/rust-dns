f3d97b66d511a6023e712df92245409ab0a87af8
Muneeb Ahmad <muneeb.gandapur@gmail.com>
2025-02-08 21:05:35 +0100

    fix: Renable resolver

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ async fn main() {
 
     setup_logger(log_level).unwrap();
 
-    let server = DnsServer::new(args.addr, args.port, None);
+    let server = DnsServer::new(args.addr, args.port, args.resolver);
 
     server.run().await
 }
